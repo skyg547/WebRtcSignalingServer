84760f4850eb9b6cfb1e97ba1269d08ea704200e
SungHo Ha <skyg547@gmail.com>
2022-01-15 16:33:35 +0900

    Initial commit

---
+++ src/main/java/com/webrtc/webrtcsignalingserver/WebRtcSignalingServerApplication.java
@@ -0,0 +1,13 @@
+package com.webrtc.webrtcsignalingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebRtcSignalingServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebRtcSignalingServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/webrtc/webrtcsignalingserver/WebRtcSignalingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webrtc.webrtcsignalingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebRtcSignalingServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
