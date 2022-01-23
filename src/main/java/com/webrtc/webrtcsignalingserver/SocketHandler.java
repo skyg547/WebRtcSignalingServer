acb4171fc14c5101e27edd2e984a39466dc0de78
SungHo Ha <skyg547@gmail.com>
2022-01-23 15:11:17 +0900

    add handler

--- src/main/java/com/webrtc/webrtcsignalingserver/SocketHandler.java
+++ src/main/java/com/webrtc/webrtcsignalingserver/SocketHandler.java
@@ -2,6 +2,7 @@ package com.webrtc.webrtcsignalingserver;
 
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
@@ -17,7 +18,7 @@ public class SocketHandler extends TextWebSocketHandler {
 
     @Override
     public void handleTextMessage(WebSocketSession session, TextMessage message) throws InterruptedException, IOException{
-
+        System.err.println("********* handle text message ***********");
         System.err.println( session.toString() + message.toString());
         for (WebSocketSession webSocketSession : sessions) {
             System.out.println(webSocketSession);
@@ -27,14 +28,27 @@ public class SocketHandler extends TextWebSocketHandler {
 
         }
 
+
+
     }
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception{
         sessions.add(session);
-        System.out.println("--------------------- conncect");
+        System.out.println("--------------------- conncect start -------");
         System.out.println(session);
     }
 
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status  ) throws Exception{
+        System.out.println("--------conect close ------");
+        sessions.remove(session);
+    }
 
+
+    @Override
+    public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
+        System.err.println("**--error--**");
+        super.handleTransportError(session, exception);
+    }
 }
