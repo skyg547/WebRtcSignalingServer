7e7bcf3597c8e0b933fc3d319024db8abdd48e38
SungHo Ha <skyg547@gmail.com>
2022-02-01 16:23:34 +0900

    make logout

--- src/main/java/com/webrtc/webrtcsignalingserver/oauthadapter/OauthAdapter.java
+++ src/main/java/com/webrtc/webrtcsignalingserver/oauthadapter/OauthAdapter.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,10 +15,15 @@ public class OauthAdapter extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests(
                         a -> a.antMatchers("/", "/error", "/webjars/**").permitAll().
-                        anyRequest().authenticated()
+                                anyRequest().authenticated()
                 )
                 .exceptionHandling(
                         e -> e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
                 ).oauth2Login();
+
+        http.logout(l -> l.logoutSuccessUrl("/").permitAll());
+
+        http.csrf(c-> c.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()));
+
     }
 }
